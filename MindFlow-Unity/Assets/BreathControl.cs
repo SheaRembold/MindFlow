482e5a8f66f01d5ae9af9ab4a2cba52eccd63b22
shea <siarck@gmail.com>
2017-01-05 17:31:12 -0500

    replaced audio clip, added start/pause/restart controls

--- MindFlow-Unity/Assets/BreathControl.cs
+++ MindFlow-Unity/Assets/BreathControl.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class BreathControl : MonoBehaviour
 {
-    void Start()
-    {
+    public AudioSource audio;
 
-    }
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!audio.isPlaying && audio.time == 0)
+                audio.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (audio.isPlaying)
+                audio.Pause();
+            else if (audio.time > 0)
+                audio.UnPause();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            audio.Stop();
+        }
     }
 }
